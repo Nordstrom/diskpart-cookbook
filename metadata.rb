297bdc01e598945037872dd8df75744a36836111
Moser, Kevin <Kevin.Moser@exchange.nordstrom.com>
2012-12-21 12:11:07 -0800

    Add README.md & metadat.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Nordstrom, Inc."
+maintainer_email "itinfrstrctrengnrng@nordstrom.com"
+license          "All rights reserved"
+description      "Installs/Configures nord-template"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
