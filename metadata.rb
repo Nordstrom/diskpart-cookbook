b8b2b6249f1ee85884cb6cb3b83cdca8f50087c1
Moser, Kevin <Kevin.Moser@exchange.nordstrom.com>
2012-12-21 12:19:07 -0800

    Update metadata information

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Nordstrom, Inc."
-maintainer_email "itinfrstrctrengnrng@nordstrom.com"
+maintainer_email "kevin.moser@nordstrom.com"
 license          "All rights reserved"
-description      "Installs/Configures nord-template"
+description      "Configures drives using diskpart"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
