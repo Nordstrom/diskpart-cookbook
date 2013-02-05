60f35d4de40a2072ea9c65bf35da1bfa5e3da04e
Moser, Kevin <Kevin.Moser@nordstrom.com>
2013-02-05 15:54:49 -0800

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "kevin.moser@nordstrom.com"
 license          "All rights reserved"
 description      "Configures drives using diskpart"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
