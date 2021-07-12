ab3f834163375827274682e586212b0f70a93e70
d032408 <bernd.kuespert@sap.com>
2021-07-12 16:18:49 +0200

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kevin.moser@nordstrom.com"
 license          "All rights reserved"
 description      "Configures drives using diskpart"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 
 supports "windows"
 depends "windows"
